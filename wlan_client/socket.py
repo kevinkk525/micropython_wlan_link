a82c7c2e385dc5b7a2f8abfe319ba0ebef4f2624
Kevin Köck <kevin.koeck@gmx.net>
2021-02-10 22:44:21 +0100

    add socket support

---
+++ wlan_client/socket.py
@@ -0,0 +1,104 @@
+# Author: Kevin Köck
+# Copyright Kevin Köck 2021 Released under the MIT license
+# Created on 2021-02-10 
+
+__updated__ = "2021-02-10"
+__version__ = "0.1"
+
+# Module based on usocket
+
+from micropython import const
+from .wclient import get_client, WlanClient
+
+SOCK_STREAM = const(1)
+AF_INET = const(2)
+
+_MAX_LEN_PAYLOAD = const(400)
+
+_CMD_GETADDRINFO = const(20)
+_CMD_GET_SOCKET = const(21)
+_CMD_CLOSE_SOCKET = const(22)
+_CMD_CONNECT_SOCKET = const(23)
+_CMD_SEND_SOCKET = const(24)
+_CMD_RECV_SOCKET = const(25)
+
+_SOCKET_TCP_MODE = const(1)
+
+
+def getaddrinfo(host: str, port: int, family=0, socktype=0, proto=0, flags=0):
+    """Given a hostname and a port name, return a 'socket.getaddrinfo'
+    compatible list of tuples. Honestly, we ignore anything but host & port"""
+    if not isinstance(port, int):
+        raise TypeError("Port must be an integer")
+    ipaddr = get_client().send_cmd_wait_answer(_CMD_GETADDRINFO, (host, port))
+    print("getaddr", ipaddr)
+    ipaddr = WlanClient.transform_args(ipaddr, str)
+    return [(AF_INET, socktype, proto, "", (ipaddr, port))]
+
+
+# TODO: handle connection exceptions
+# TODO: implement timeout with blocking socket
+
+class socket:
+    def __init__(self, family=AF_INET, type=SOCK_STREAM, proto=0,
+                 fileno=None, socknum=None):
+        if family != AF_INET:
+            raise TypeError("Only AF_INET family supported")
+        if type != SOCK_STREAM:
+            raise TypeError("Only SOCK_STREAM type supported")
+        self._buffer = b""
+        self._socknum = socknum if socknum else get_client().send_cmd_wait_answer(_CMD_GET_SOCKET)
+        self._timeout = None  # None=blocking without timeout, 0=non-blocking
+        self._blocking = True
+        self._closed = False
+        print(self._socknum)
+
+    def close(self):
+        get_client().send_cmd_wait_answer(_CMD_CLOSE_SOCKET, self._socknum)
+        self._closed = True
+
+    def setblocking(self, blocking: bool):
+        self._blocking = blocking
+
+    def connect(self, address, conntype=None):
+        """Connect the socket to the 'address' (which can be 32bit packed IP or
+        a hostname string). 'conntype' is an extra that may indicate SSL or not,
+        depending on the underlying interface"""
+        host, port = address
+        if conntype is None:
+            conntype = _SOCKET_TCP_MODE
+        get_client().send_cmd_wait_answer(_CMD_CONNECT_SOCKET,
+                                          (self._socknum, host, port, conntype, self._blocking),
+                                          timeout=30000 if self._blocking else 1000)
+        self._buffer = b""
+
+    def send(self, data) -> int:
+        """Send some data to the socket"""
+        if len(data) > _MAX_LEN_PAYLOAD:
+            raise ValueError("Payload too long")  # could split it up but good for now.
+        if len(data) > 255:  # limited to 255 because of 1 byte for param length in param header
+            d = [self._socknum]
+            c = 0
+            if type(data) != memoryview:
+                data = memoryview(data)
+            while c < len(data):
+                d.append(data[c:c + 255])
+                c += 255
+        else:
+            d = (self._socknum, data)
+        return get_client().send_cmd_wait_answer(_CMD_SEND_SOCKET, d)
+
+    def recv(self, bufsize=0):
+        if bufsize == 0:
+            return b''
+        elif bufsize > _MAX_LEN_PAYLOAD:
+            bufsize = _MAX_LEN_PAYLOAD  # let application handle shorter reads.
+        d = get_client().send_cmd_wait_answer(_CMD_RECV_SOCKET,
+                                              (self._socknum, bufsize, self._blocking),
+                                              timeout=None if self._blocking else 1000)
+        return bytes(d)  # can't return memoryview as this is the client's buffer
+
+    def __del__(self):
+        """Just in case?"""
+        if not self._closed:
+            self.close()
---
+++ wlan_host/socket.py
@@ -0,0 +1,188 @@
+# Author: Kevin Köck
+# Copyright Kevin Köck 2021 Released under the MIT license
+# Created on 2021-02-10 
+
+__updated__ = "2021-02-10"
+__version__ = "0.1"
+
+from micropython import const
+from .whost import get_host
+from .command_handler import wlanHandler
+from wlan_host.whost import WlanHost
+import usocket
+import gc
+import errno
+import sys
+
+_MAX_LEN_PAYLOAD = const(400)
+
+_CMD_GETADDRINFO = const(20)
+_CMD_GET_SOCKET = const(21)
+_CMD_CLOSE_SOCKET = const(22)
+_CMD_CONNECT_SOCKET = const(23)
+_CMD_SEND_SOCKET = const(24)
+_CMD_RECV_SOCKET = const(25)
+
+_SOCKET_TCP_MODE = const(1)
+
+
+@wlanHandler.register(_CMD_GETADDRINFO)
+def getaddrinfo(wl: WlanHost, host: str, port: int, family=0, socktype=0, proto=0, flags=0):
+    host = bytes(host).decode()
+    port = int(bytes(port))
+    print("getaddrinfo", host, port, family, socktype, proto, flags)
+    try:
+        return True, usocket.getaddrinfo(host, port, family, socktype, proto, flags)[0][4][0]
+    except Exception as e:
+        print(e)
+        return e
+
+
+def socknum_gen():
+    pid = 0
+    while True:
+        pid = pid + 1 if pid < 65535 else 1
+        yield pid
+
+
+class Sockets:
+    _sockets = {}
+    _newpid = socknum_gen()
+    max_sockets = 16  # ESP32 raises Exception with more than 5 sockets?
+    active_sockets = 0
+    socket_rx_buffer = 400  # rx buffer for socket object
+
+    @staticmethod
+    @wlanHandler.register(_CMD_GET_SOCKET)
+    def create_socket(wl: WlanHost, *args):
+        if Sockets.active_sockets >= Sockets.max_sockets:
+            if wl._debug >= 1:
+                print("Maximum configured sockets reached")
+            return OSError(23)
+        try:
+            s = usocket.socket()
+        except Exception as e:
+            sys.print_exception(e)
+            return e
+        pid = next(Sockets._newpid)
+        while pid in Sockets._sockets:
+            pid = next(Sockets._newpid)
+        Sockets._sockets[pid] = socket(s, pid, Sockets.socket_rx_buffer)
+        return True, pid
+
+    @staticmethod
+    def _get_socket(socknum):
+        if socknum in Sockets._sockets:
+            return Sockets._sockets[socknum]
+        else:
+            raise TypeError("Socket {} does not exist".format(socknum))
+
+    @staticmethod
+    def _remove_socket(socknum):
+        del Sockets._sockets[socknum]
+
+    @staticmethod
+    @wlanHandler.register(_CMD_CONNECT_SOCKET)
+    def connect(wl: WlanHost, socknum, host, port, conntype, blocking):
+        socknum = wl.transform_args(socknum, int)
+        host = wl.transform_args(host, str)
+        port = wl.transform_args(port, int)
+        conntype = wl.transform_args(conntype, int)
+        blocking = wl.transform_args(blocking, bool)
+        print("connect", socknum, host, port, conntype, blocking)
+        sock = Sockets._get_socket(socknum)
+        return sock.connect(host, port, conntype, blocking)
+
+    @staticmethod
+    @wlanHandler.register(_CMD_CLOSE_SOCKET)
+    def close(wl: WlanHost, socknum):
+        socknum = wl.transform_args(socknum, int)
+        try:
+            sock = Sockets._get_socket(socknum)
+        except TypeError:
+            print("Socket already removed", socknum)
+            return True
+        sock.close()
+        Sockets._remove_socket(socknum)
+        del sock
+        gc.collect()
+        print("Closed socket", socknum)
+        return True
+
+    @staticmethod
+    @wlanHandler.register(_CMD_SEND_SOCKET)
+    def send(wl: WlanHost, socknum, *args):
+        socknum = wl.transform_args(socknum, int)
+        try:
+            sock = Sockets._get_socket(socknum)
+        except TypeError:
+            print("Socket doesn't exist", socknum)
+            return OSError, errno.ECONNRESET
+        return sock.send(*args)
+
+    @staticmethod
+    @wlanHandler.register(_CMD_RECV_SOCKET)
+    def recv(wl: WlanHost, socknum, bufsize, blocking):
+        socknum = wl.transform_args(socknum, int)
+        bufsize = wl.transform_args(bufsize, int)
+        blocking = wl.transform_args(blocking, bool)
+        try:
+            sock = Sockets._get_socket(socknum)
+        except TypeError:
+            print("Socket doesn't exist", socknum)
+            return OSError, errno.ECONNRESET
+        return sock.recv(bufsize, blocking)
+
+
+class socket:
+    def __init__(self, sock: usocket, socknum: int, len_buffer: int):
+        self._socknum = socknum
+        self._sock = sock
+        self._buffer = bytearray(len_buffer)
+        self._conntype = None
+
+    def connect(self, host: str, port: int, conntype: int, blocking: bool):
+        print("Connecting")
+        self._conntype = conntype
+        self._sock.setblocking(blocking)
+        try:
+            self._sock.connect((host, port))
+            print("Connected")
+            return True
+        except OSError as e:
+            return e
+        finally:
+            if blocking:
+                self._sock.setblocking(False)  # internally we'll use non-blocking sockets
+
+    def close(self):
+        self._sock.close()
+        return True
+
+    def send(self, *args):
+        cnt = 0
+        for arg in args:
+            try:
+                cnt += self._sock.send(arg)
+            except Exception as e:
+                return e
+        return True, cnt
+
+    def recv(self, bufsize, blocking):
+        # for now blocking=True will freeze the esp32 which is not desirable.
+        self._sock.setblocking(blocking)
+        try:
+            data = self._sock.recv(bufsize)
+        except Exception as e:
+            sys.print_exception(e)
+            return e
+        if len(data) > 255:  # limited to 255 because of 1 byte for param length in param header
+            d = [True]
+            c = 0
+            if type(data) != memoryview:
+                data = memoryview(data)
+            while c < len(data):
+                d.append(data[c:c + 255])
+                c += 255
+            return d
+        return True, data
